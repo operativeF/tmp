395c43cbf7262e05bbba4f8541bb055ee034f6d8
Thomas Figueroa <tom_figueroa@hotmail.com>
2019-07-25 16:28:59 -0500

    WIP / Mess

--- test/any_of_test.hpp
+++ test/any_of_test.hpp
@@ -17,40 +17,25 @@ namespace any_of_test {
     template <typename T>
 	using is_even = bool_<(T::value % 2 == 0)>;
 
-    using alist = list_<int_<1>, int_<1>>;
+    template <typename T, typename U>
+	using plus_ = int_<(T::value + U::value)>;
+
+    template<typename T, typename U>
+    using same_as = call_<is_<T>, U>;
+
+    using alist = list_<int_<1>>;
     using arslist = list_<int_<1>, int_<1>>;
 
-    template<typename P, typename F, typename S, typename C = listify_>
-    using whilst_ = transform_<if_<is_<S, and_<P>>, F, identity_>, join_<C>>;
-
-    using wlist = call_<unpack_<whilst_<is_<int_<1>>, erase_<>, continue_>>, alist>;
-
-    template<typename F, typename C = listify_>
-    using erase_if_ = transform_<if_<F, erase_<>, listify_>, join_<C>>;
-
-    using blist = call_<unpack_<erase_if_<lift_<is_even>>>, alist>;
-
-    using clist = call_<join_<>, call_<unpack_<erase_if_<lift_<is_even>>>, alist>>;
-
-    // This is important. For flatten_ to work properly here, we have to join the results; otherwise, they're all separate and can't be represented
-    // by the type system.
+    // This is important. For flatten_ to work properly here, we have to join the results; otherwise, it's just a variadic parameter pack floating around.
     using dlist = call_<product_<flatten_<>, join_<>>, alist, alist>;
-/*
-    using elist = call_<
-        product_<
-            flatten_<
-                push_front_<list_<>,
-                >,
-            join_<>
-        >, arslist, arslist>;
-*/
+
+    using rlist = call_<fold_left_< each_<unpack_<fold_left_<lift_<same_as>>>, unpack_<fold_left_<lift_<same_as>>>, lift_<same_as>> >, alist, alist>;
+
 	int run() {
 
-        blist{} = list_<int_<1>, int_<1>>{};
-        clist{} = list_<int_<1>, int_<1>>{};
-        wlist{} = list_<>{};
+        rlist{} = true_{};
 
-        //elist{} = list_<int_<1>, int_<1>, int_<1>, int_<1>, int_<1>, int_<1>, int_<1>, int_<1>>{};
+        dlist{} = list_<int_<1>, int_<1>>{};
 
         //bool_<true>{} = call_<unpack_<any_of_<lift_<is_even>>>, alist>{};
 		//bool_<false>{} = call_<unpack_<any_of_<lift_<is_even>>>, blist>{};
